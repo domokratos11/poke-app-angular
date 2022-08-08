ae4306e475ffb1018eba84b6779f86ce41e2f47c
domokratos11 <reynaldodiaz11@gmail.com>
2022-08-08 15:55:58 -0600

    Search bar added, pending state fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'poke-app-angular';
+  searchText?: string;
+
+  doSearch(searchText: string): void {
+    this.searchText = searchText;
+  }
 }
--- src/app/components/poke-card/poke-card-list.component.ts
+++ src/app/components/poke-card/poke-card-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  DoCheck,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ConfigService, PokemonResult } from '../../config/config.service';
 
 @Component({
@@ -6,12 +13,20 @@ import { ConfigService, PokemonResult } from '../../config/config.service';
   templateUrl: './poke-card-list.component.html',
   providers: [ConfigService],
 })
-export class PokeCardList implements OnInit {
+export class PokeCardList implements OnInit, OnChanges {
   response: PokemonResult | undefined;
-
+  @Input() searchText?: string;
   constructor(private configService: ConfigService) {}
 
   ngOnInit(): void {
+    this.SearchPokemons();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['searchText'].firstChange && this.searchText)
+      this.searchText =
+        this.configService.fetchPokemonURL + this.searchText?.toLowerCase();
+  }
+  SearchPokemons(): void {
     this.configService.getPokemons().subscribe((data: PokemonResult) => {
       this.response = { ...data };
     });
